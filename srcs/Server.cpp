e243ad40278a0ace50de2e35a5a1d90ff75ef558
Yeji Choi <yechoi@student.42seoul.kr>
2021-04-20 14:20:14 +0900

    [FIX] read buff memset

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -255,7 +255,7 @@ Server::setServerSocket()
 		std::cout << "bind error" << std::endl;
 		return (false);
 	}
-	if (listen(this->m_server_socket, 1000) == -1)
+	if (listen(this->m_server_socket, 10) == -1)
 	{
 		std::cout << "listen error" << std::endl;
 		return (false);
@@ -635,8 +635,6 @@ Server::writeProcess()
 					std::cout << "O" << EPIPE << std::endl;
 					std::cout << "P" << EDESTADDRREQ << std::endl;
 				}
-
-
 				if (ret <= 0)
 				{
 					static int youpiget = 0;
